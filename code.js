112a1ff8f47d1131099ba5bbc136665f7af4fd19
VusetOrucov <58605237+VusetOrucov@users.noreply.github.com>
2021-01-14 13:20:00 +0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,18 @@
+var n=300;
+var k=(location.hash || "").substr(1),
+    k=k.replace(/[\?&].*/g, "");
+function gb(a,c,b){
+    var e=new XMLHttpRequest,arr=[],indf;
+    for(ind in c) arr.push(encodeURIComponent(ind)+"="+encodeURIComponent(c[ind]));
+    e.onreadystatechange=function(){
+        4==e.readyState && 200==e.status && b(JSON.parse(e.responseText))
+    };
+    e.open("POST",a,!0);
+    e.send(arr.join("&"))
+}
+gb("/api/setScore",{
+    data:k,score:n || 0
+},function(a){
+    console.log("Success");
+    console.log(a);
+});
